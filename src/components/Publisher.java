210a1e2e48e0a0693595f37cbcf5b723ba9daf2b
RozovykArtemiy <rozovyk.artem@gmail.com>
2020-02-08 12:24:12 +0100

    diff to see the modifications by Mathieu,

--- src/components/Publisher.java
+++ src/components/Publisher.java
@@ -1,6 +1,7 @@
 package components;
 
 import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.cvm.AbstractCVM;
 import interfaces.MessageI;
 import ports.PublisherPublicationOutboundPort;
 
@@ -8,17 +9,33 @@ public class Publisher extends AbstractComponent{
 	
 	protected PublisherPublicationOutboundPort ppop;
 	
-	protected String publisherPublicationOutboundPortURI;
+	//protected String publisherPublicationOutboundPortURI;
 	
 	public Publisher(int nbThreads, int nbSchedulableThreads) {
 		super(nbThreads, nbSchedulableThreads);
 		// TODO Auto-generated constructor stub
 	}
 	
-	protected Publisher (String publisherPublicationOutboundPortURI) {
-		super(publisherPublicationOutboundPortURI,1,0);
-		this.publisherPublicationOutboundPortURI=publisherPublicationOutboundPortURI;
+	public Publisher(String uri, 
+			String publicationOutboundPortURI) throws Exception
+	{
+		super(uri, 0, 1) ;
+		
+		//Publish the reception port (an outbound port is always local)
+		this.ppop = new PublisherPublicationOutboundPort(publicationOutboundPortURI, this);
+		this.ppop.localPublishPort();
+		
+		if (AbstractCVM.isDistributed) {
+			this.executionLog.setDirectory(System.getProperty("user.dir")) ;
+		} else {
+			this.executionLog.setDirectory(System.getProperty("user.home")) ;
+		}
+		
+		this.tracer.setTitle("publisher") ;
+		this.tracer.setRelativePosition(1, 0) ;
 	}
+	
+	
 	@Override
 	public void execute() throws Exception{
 		publish(new Message("Hello bro"), "nothing");
