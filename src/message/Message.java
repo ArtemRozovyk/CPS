9b8ba091c1effe26ef1c8ee0d0bcdc662b767e2a
GEORGE <28605813@ppti-14-407-12.ufr-info-p6.jussieu.fr>
2020-02-10 16:18:18 +0100

    message methods

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,15 +1,17 @@
 package message;
+
 import java.io.Serializable;
 
 import interfaces.MessageI;
 
 public class Message implements MessageI {
-	
-	
+
 	private static final long serialVersionUID = -2687757832261453813L;
-	
+
 	private Serializable content;
+
 	private TimeStamp ts;
+
 	private String URI;
 
 	public Message(Serializable content, TimeStamp ts, String URI, Properties prop) {
@@ -24,40 +26,30 @@ public class Message implements MessageI {
 	public Message(String msg) {
 		super();
 	}
-	
 
-
-	
 	@Override
 	public String toString() {
-		return "Message{"+ts.timestamper;
+		return "Message{" + ts.timestamper;
 	}
 
 	@Override
 	public String getURI() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.URI;
 	}
 
 	@Override
 	public TimeStamp getTimeStamp() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.ts;
 	}
 
 	@Override
 	public Properties getProperties() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prop;
 	}
 
 	@Override
 	public Serializable getPayload() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.content;
 	}
-
-
-
 
 }
